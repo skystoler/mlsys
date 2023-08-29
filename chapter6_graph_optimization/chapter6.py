1eb0185fdef232bbe5ca2d9209462462e1a8dae0
skystoler <982932529@qq.com>
2023-08-29 17:39:21 +0800

    add chapter6_graph_optimization

---
+++ chapter6_graph_optimization/chapter6.py
@@ -0,0 +1,50 @@
+import numpy as np
+import tvm
+from tvm import relax, topi
+from tvm.ir.module import IRModule
+from tvm.script import relax as R
+from tvm.script import tir as T
+
+
+@tvm.script.ir_module
+class MyModule:
+    @R.function
+    def main(x: R.Tensor((3, 4), "float32"), y: R.Tensor((3, 4), "float32")):
+        with R.dataflow():
+            lv0 = relax.op.multiply(x, y)
+            gv0 = relax.op.add(lv0, y)
+            R.output(gv0)
+        return gv0
+
+
+# We use visitor pattern to visit every AST node recursively and rewrite them
+@relax.expr_functor.mutator
+class EwiseFMARewriter(relax.PyExprMutator):
+    def visit_call_(self, call):
+        call = self.visit_expr_post_order(call)
+        add_op = tvm.ir.Op.get("relax.add")
+        multiply_op = tvm.ir.Op.get("relax.multiply")
+        ewise_fma_op = tvm.ir.Op.get("relax.ewise_fma")
+
+        if call.op != add_op:
+            return call
+
+        value = self.loopup_binding(call.args[0])
+        if not isinstance(value, relax.Call) or value.op != multiply_op:
+            return call
+
+        fma_call = relax.Call(
+            ewise_fma_op, [value.args[0], value.args[1], call.args[1]], None, None
+        )
+        return fma_call
+
+
+def test_graph_optimization():
+    updated_fn = EwiseFMARewriter().visit_expr(MyModule["main"])
+    updated_fn.show()
+
+    relax.analysis.remove_all_unused(updated_fn).show()
+
+
+if __name__ == "__main__":
+    test_graph_optimization()
